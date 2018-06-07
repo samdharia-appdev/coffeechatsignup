a5d94d0bf71aa192a7b9b0effc11eddf73093e4d
Raghu Betina <raghu@firstdraft.com>
2018-06-07 15:16:04 +0000

    Add root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root :to => "companies#index"
   # Routes for the Availability resource:
   # CREATE
   get "/availabilities/new", :controller => "availabilities", :action => "new"
